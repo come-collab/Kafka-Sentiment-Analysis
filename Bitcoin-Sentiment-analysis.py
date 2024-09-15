9885fe772b4b4caed0b1ac961bfbb63ca49f3780
come-collab <cometrisse640@gmail.com>
2024-09-15 15:30:20 +0200

    added the Reddit keys

---
+++ Bitcoin-Sentiment-analysis.py
@@ -0,0 +1,10 @@
+import praw
+
+
+#Setting up the Reddit API
+reddit = praw.Reddit(
+    client_id='vcvPFQWZnZ5boS_GdaWfdg',
+    client_secret='4-CZKEgmQznkTqF_LVNMskj2rAg8nQ',
+    user_agent='my-app by Shot-Taste5816'
+)
+
